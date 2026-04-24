f83724504d44586c280a1adf1fd9d1c8fe551461
Anton J Olsson <ao@antolsson.se>
2026-04-24 00:00:00 +0000

    feat: 🎸 save in db and create hash

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const { nanoid } = require('nanoid')
+const MAX_ALLOWED_URLS = 10e+9
 
 const app = express()
 const port = 48985
@@ -9,28 +12,52 @@ const cors = require('cors')
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!')
-    console.log(`Request received! ${req}`)
+    res.send('Hello from AutoSFC server!')
+    console.info(`Request received! ${req}`)
 })
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`AutoSFC backend listening on port ${port}!`)
+    console.info(`AutoSFC server listening on port ${port}!`)
 })
 
 app.post('/', bodyParser.json(), (req, res) => {
     const url = req.body.url
-    console.log(`Request received from ${req.headers.origin}!`)
+    console.info(`Request received from ${req.headers.origin}!`)
 
-    request(url, {}, (err, response, body) => {
+    request(url, {}, async (err, response, body) => {
         if (err) {
             console.error(err)
             res.status(400).json({error: `Express: Error when loading URL: ${err}`})
         } else {
-            console.log('Fetch worked! Data:')
+            console.info('Fetch worked! Data:')
             let lines = body.toString().split('\n')
             lines = lines.length <= 3 ? lines.join('\n') : lines.slice(0, 3).join('\n') + '\n...'
-            console.log(lines)
-            res.status(200).json({msg: 'Express: File read successfully!', fileContent: body})
+            console.info(lines)
+
+            // Retrieve or generate hash
+            let newUid = false
+            let uid = ''
+
+            const defaultData = {urls: []}
+            const {JSONFilePreset} = await import ('lowdb/node')
+            const db = await JSONFilePreset('db.json', defaultData)
+            const { urls } = db.data
+            // noinspection JSUnresolvedReference
+            const existingUrl = urls.find(u => u.fullUrl === url)
+            if (existingUrl) {
+                uid = existingUrl.uid
+                console.info(`Found URL ${url} in db; UID: ${uid}`)
+            } else {
+                uid = nanoid(8)
+                await db.update(({urls}) => urls.push({fullUrl: url, uid: uid}))
+                console.info(`URL ${url} not found in db; Uid generated and added to db: ${uid}`)
+                newUid = true
+            }
+
+            res.status(200).json({
+                msg: `Express: File read successfully! ${newUid ? `New UID created: ${uid}`: `UID received: ${uid}`}.`,
+                fileContent: body, uid: uid, newUid: newUid
+            })
         }
     })
 })
