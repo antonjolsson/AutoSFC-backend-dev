9f8147b197e93852bce894cbd05677fdf608b8e4
Anton J Olsson <ao@antolsson.se>
2026-04-24 00:00:00 +0000

    feat: 🎸 receive file urls and return content

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
 const app = express()
 const port = 48985
 const cors = require('cors')
@@ -11,5 +14,23 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`AutoSFC backend listening on port ${port}!`)
+})
+
+app.post('/', bodyParser.json(), (req, res) => {
+    const url = req.body.url
+    console.log(`Request received from ${req.headers.origin}!`)
+
+    request(url, {}, (err, response, body) => {
+        if (err) {
+            console.error(err)
+            res.send(`Express: Error when loading URL: ${err}`)
+        } else {
+            console.log('Fetch worked! Data:')
+            let lines = body.toString().split('\n')
+            lines = lines.length <= 3 ? lines.join('\n') : lines.slice(0, 3).join('\n') + '\n...'
+            console.log(lines)
+            res.send({msg: 'Express: File read successfully!', fileContent: body})
+        }
+    })
 })
